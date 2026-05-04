9e57a345702d49c8361a88999f27fc42228c1e2b
Krishna Kasyap <kanuparthikasyap95@gmail.com>
2026-05-04 22:43:18 +0530

    added codes

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ from app.adminblogs import admin_app
 from app.analysis import analysis_app
 from app.blogs import blogs_app
 from fastapi.middleware.cors import CORSMiddleware
+import threading
+import time
+import requests
 
 # Create main FastAPI app
 app = FastAPI(title="Main Application with mounts")
@@ -33,6 +36,28 @@ async def main_root():
         }
     }
 
+URL = "https://nyaya-backend-muktha.onrender.com/ping"  # self endpoint
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "I'm alive!"}
+
+
+def self_ping():
+    while True:
+        try:
+            res = requests.get(URL)
+            print("Pinged:", res.status_code, res.json())
+        except Exception as e:
+            print("Error:", e)
+        time.sleep(30)
+
+
+@app.on_event("startup")
+def start_self_ping():
+    thread = threading.Thread(target=self_ping, daemon=True)
+    thread.start()
 
 if __name__ == "__main__":
     import uvicorn
